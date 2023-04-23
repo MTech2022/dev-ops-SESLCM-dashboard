3ffe4da633809a7d1b30709b75faf309168c2824
Sandeep M <j.sandeepm@gmail.com>
2023-04-23 10:31:07 +0530

    login page changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {MarksDashboardComponent} from './marks-dashboard/marks-dashboard.component';
+import { MarksDashboardComponent } from './marks-dashboard/marks-dashboard.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: 'dashboard', component: MarksDashboardComponent },
-  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-  {path: '**', redirectTo: '/dashboard', pathMatch: 'full'}
+  {
+    path: 'dashboard',
+    component: MarksDashboardComponent,
+  },
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', redirectTo: '/login', pathMatch: 'full' }
 ];
 
 @NgModule({
